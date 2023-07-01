de33211d4ee6fd978458c0b447c0754e62ce7455
DeveKoke <jorge.be86@gmail.com>
2023-07-01 19:22:50 +0200

    messages by useEffect

--- REACT-ToDo_List/src/components/Main/List/List.jsx
+++ REACT-ToDo_List/src/components/Main/List/List.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect} from 'react';
 import Card from './Card';
 import './List.css';
 
@@ -6,17 +6,24 @@ const List = () => {
 
   const [tasks, setTasks] = useState([]); //en este [] se acumularán los valores que acumule 'tasks'
   const [originalTasks, setOriginalTasks] = useState([]);  // En este [] se guarda una copia del estado anterior.
+  const [showMessage, setShowMessage] = useState(false);  //Estado inicial de showmessage es false (no se muestra en pantalla)
+  const [errorMessage, setErrorMessage] = useState('');
+
     const handleSubmit = (e) => {
       e.preventDefault();
       const task = e.target.task.value;
-      
-      // Agrega la nueva tarea al array tasks
-      setTasks(prevTasks => [...prevTasks, task]);
 
+          if (task.length < 6) {  //si al hacer click task es menor de 6 setErroMessage cambia de estado
+            setErrorMessage('La tarea debe tener al menos 6 caracteres');
+            return;
+          }
+      
+      setTasks(prevTasks => [...prevTasks, task]);  // Agrega la nueva tarea al array tasks
       setOriginalTasks(prevTasks => [...prevTasks, task]);
-    
-      // Limpia el valor del input después de agregar la tarea
-      e.target.task.value = '';
+      setShowMessage(true); //si se hace click en ADD TASK, showMessage cambia valor.
+      setErrorMessage('');
+      
+      e.target.task.value = '';  // Limpia el valor del input después de agregar la tarea
   }
 
   const deleteTask = (i) =>{setTasks(tasks.filter( (item, j)=> i!==j )) }
@@ -29,16 +36,36 @@ const List = () => {
     return tasks.map((task, index) => (<Card key={index} task={task} deleteTask = { () => deleteTask(index)} />
     ));
   }
-// console.log(tasks);
+
+  useEffect(() => {  
+    if (errorMessage) {  //si errorMessage está true, se camba a false en 3 seg.
+      const timeout = setTimeout(() => {
+        setErrorMessage(false);
+      }, 10000);
+      return () => clearTimeout(timeout);
+    }
+  }, [errorMessage]); 
+
+  
+  useEffect(() => {  
+    if (showMessage) {  //si showMessage está true, se camba a false en 3 seg.
+      const timeout = setTimeout(() => {
+        setShowMessage(false);
+      }, 10000);
+      return () => clearTimeout(timeout);
+    }
+  }, [showMessage]); //el array es un parámetro de useEffect que indica que este se ejecutará cuando showMessage cambie su estado.
 
     return (
           <article>
             <form onSubmit={handleSubmit}>
-              
+            {showMessage && <p className='taskmessage'>Tarea añadida</p>}
+            {errorMessage && <p className="taskmessage">{errorMessage}</p>}
+
               <label htmlFor="name">Type your task</label><br />
               <input type="text" name="task" /><br />
           
-              <button type="submit">ADD TASK</button>
+              <button id='addbtn' type="submit">ADD TASK</button>
 
             </form>
             <ul>
